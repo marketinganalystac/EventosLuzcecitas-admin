eec325187874e1f6511dd3c8f7ba1f139b57ceb9
marketinganalystac <marketing.analyst.ac@gmail.com>
2026-01-10 11:24:07 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // Asegúrate de que tu archivo App se llame exactamente así
+import './index.css'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
