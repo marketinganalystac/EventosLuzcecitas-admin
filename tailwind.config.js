fc6ae3bcffba50d2afe9f205482ef9fbf29837d7
marketinganalystac <marketing.analyst.ac@gmail.com>
2026-01-10 11:31:55 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Le dice a Tailwind en qué archivos buscar las clases de colores y diseño
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
