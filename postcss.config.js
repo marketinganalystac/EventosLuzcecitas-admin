f9ae339a9b5f3db548cc0c82fba579ac6a0d8dc3
marketinganalystac <marketing.analyst.ac@gmail.com>
2026-01-10 12:12:03 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  // Aquí le indicas a Tailwind que revise el index.html y todos los archivos 
-  // dentro de la carpeta 'src' que terminen en .js o .jsx
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      // Aquí podrías agregar colores personalizados si quisieras más adelante
-    },
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
 }
